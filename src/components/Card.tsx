c98cfb537fc62d0a564a49ff6f0d0e11fff57966
xoma_star <sasha.sirotyin09@gmail.com>
2022-09-05 12:28:09 +0500

    init

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,7 @@ const Card = () => {
     const cardNo = localStorage.getItem('cardNo')
 
     const addCardHandler = () => {
+        bridge.send('VKWebAppTapticImpactOccurred', {style: 'light'})
         bridge.send('VKWebAppOpenQR').then(r => console.log(r))
     }
 
@@ -13,7 +14,7 @@ const Card = () => {
         <div className={'rounded-3xl bg-gradient-to-br from-[#FDABDD] to-[#374A5A] mx-6 mt-12 p-3 h-fit max-w-[460px]'}>
             {cardNo ?
                 <img src={alga} className={'rounded-3xl'}/> :
-                <div onClick={addCardHandler} role={'button'} className={'bg-pink-100 rounded-3xl text-center py-4 font-bold'}>Добавить карту</div>
+                <div onClick={addCardHandler} className={'bg-pink-100 rounded-3xl text-center py-4 font-bold'}>Добавить карту</div>
             }
             {cardNo && <div className={'font-bold text-center mt-3 text-rose-50'}>{cardNo}</div>}
         </div>
